67399598459878f96c100123e941cdda0234bba2
2971392411036 <2971392411036@LAB01-D14>
2025-02-24 12:36:18 -0300

    Criação Básica do dominios e entidades.

---
+++ HelpApp/HelpApp.Domain/Entities/Category.cs
@@ -0,0 +1,33 @@
+﻿using HelpApp.Domain.Validation;
+using System.Collections.Generic;
+
+namespace HelpApp.Domain.Entities
+{
+    public class Category
+    {
+        #region Atributos
+        public int Id { get; set; }
+        public string Name { get; set; }
+        #endregion
+
+        #region Constructors
+        public Category(string name)
+        {
+            Name = name;
+        }
+
+        public ICollection<Product> Products { get; set; }
+        #endregion
+        #region Validação
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Invalid name, name is required");
+            DomainExceptionValidation.When(Name.Length < 3,
+                "Invalid name, too short, minum 3 characters. ");
+
+            Name = name;
+        }
+        #endregion
+    }
+}
