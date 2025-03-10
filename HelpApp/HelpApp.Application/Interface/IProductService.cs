c17214bd0786225c8923534a8ae253e3b854abca
VICTOR ICOMA <victorsena3010@gmail.com>
2025-03-10 11:53:43 -0300

    Aula de estrutura

---
+++ HelpApp/HelpApp.Application/Interface/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpApp.Application.Interface
+{
+    internal class IProductService
+    {
+    }
+}
