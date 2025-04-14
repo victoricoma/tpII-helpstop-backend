307f4970905d015f178365a8b25e2e198c15a120
VICTOR ICOMA <victorsena3010@gmail.com>
2025-04-14 12:48:28 -0300

    Aula1404 - Conectores base

---
+++ HelpApp/HelpApp.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using HelpApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HelpApp.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        }
+    }
+}
