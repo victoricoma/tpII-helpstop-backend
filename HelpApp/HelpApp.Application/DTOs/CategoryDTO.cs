b2e02d875e89e174969a255352e66f4a22470915
carol-silva205 <carolineolvs0@gmail.com>
2025-05-12 12:47:30 -0300

    Aula 12/05

---
+++ HelpApp/HelpApp.Application/DTOs/CategoryDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HelpApp.Application.DTOs
+{
+	public class CategoryDTO
+	{
+		public int Id { get; set; }
+		[Required(ErrorMessage = "Invalid name, name is required.")]
+		[MinLength(3)]
+		[MaxLength(100)]
+		public string Name { get; set; }
+		[Required(ErrorMessage = "Invalid name, too short, minimum 3 characters.")]
+
+	}
+}
