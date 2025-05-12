7ab3b4b870e95e326e1d19bdb9f04a6ad3e442eb
VICTOR ICOMA <victorsena3010@gmail.com>
2025-05-12 12:45:21 -0300

    Aula de 12/05 - Dtos e Interfaces

---
+++ HelpApp/HelpApp.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using HelpApp.Domain.Entities;
+using HelpApp.Application.DTOs;
+
+namespace HelpApp.Application.Mappings
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile() 
+        {
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<Product, ProductDTO>().ReverseMap();
+        }
+    }
+}
